29aa4c114b3dfe155c8db74dc99c079a1ba45fad
xiaolala <hjgguge@gmail.com>
2018-10-04 13:52:12 +0800

    ss

---
+++ hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello
+//
+//  Created by developer on 2018/10/4.
+//  Copyright © 2018 developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hello/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  hello
+//
+//  Created by developer on 2018/10/4.
+//  Copyright © 2018 developer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
